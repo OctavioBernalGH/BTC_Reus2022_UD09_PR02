6bc541ef5231714af4a684fd9e78ddf39796506d
mednologic <mednologic@gmail.com>
2022-04-16 10:16:40 +0200

    "Updating Estructure"

--- BTC_Reus2022_PR02/src/App/MainApp.java
+++ BTC_Reus2022_PR02/src/App/MainApp.java
@@ -1,10 +1,16 @@
 package App;
 
+/*
+ * @author:		Octavio Bernal Vilana / David Dalmau Dieguez / Josep Oriol Lopez Bosch
+ * @fecha:		16/04/2022
+ * @versi�n:	0.0.1
+ */
+
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
---
+++ BTC_Reus2022_PR02/src/Class/Entregable_Inter.java
@@ -0,0 +1,5 @@
+package Class;
+
+public interface Entregable_Inter {
+
+}
